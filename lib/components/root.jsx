3c2368158d4228449158e95266a38d05d8f613d6
Andrew Booth <booth@shipt.com>
2017-10-21 01:17:03 -0700

    Populate tree info

--- lib/components/root.jsx
+++ lib/components/root.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { connect, Provider } from 'react-redux';
-import { updatePosition } from '../actions';
+import { updatePosition, updateTrees } from '../actions';
 import Map from './map';
 
-const mapStateToProps = state => ({});
+const mapStateToProps = ({ position }) => ({
+  position
+});
 
 const mapDispatchToProps = dispatch => ({
-  updatePosition: () => dispatch(updatePosition())
+  updatePosition: () => dispatch(updatePosition()),
+  updateTrees: position => dispatch(updateTrees(position))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(
   class extends React.Component {
     constructor(props) {
       super(props);
-      this.props.updatePosition();
+
+      this.props.updatePosition().then(
+        () => this.props.updateTrees(this.props.position)
+      );
     }
 
     render() {
